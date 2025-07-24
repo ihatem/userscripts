6c6cbac687e68d49fca4b1e2f23fef8b5f712de6
ihatem <zehani.hatem@gmail.com>
2025-07-24 21:43:33 +0200

    fix: update match pattern to ensure compatibility with YouTube

--- youtube_sidebar_expanded.user.js
+++ youtube_sidebar_expanded.user.js
@@ -4,13 +4,12 @@
 // @version      1.0.0
 // @description  Force YouTube sidebar to always open
 // @author       @ihatem
-// @match        https://*.youtube.com/*
+// @match        https://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @homepage     https://github.com/ihatem
 // @homepageURL  https://github.com/ihatem
 // @supportURL   https://github.com/ihatem/userscripts/issues
 // @run-at       document-idle
-// @require      https://cdn.jsdelivr.net/gh/CoeJoder/waitForKeyElements.js@v1.3/waitForKeyElements.js
 // @updateURL    https://raw.githubusercontent.com/ihatem/userscripts/refs/heads/main/youtube_sidebar_expanded.user.js
 // @downloadURL  https://raw.githubusercontent.com/ihatem/userscripts/refs/heads/main/youtube_sidebar_expanded.user.js
 // @grant        none
